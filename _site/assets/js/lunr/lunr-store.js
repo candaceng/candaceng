3d432849dcf886b283bd1c9f66b13d10459644ea
candaceng <candacewyng@gmail.com>
2020-09-05 14:47:23 -0400

    configuration

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Virtual Trading Application",
+        "excerpt":"GitHub Repo About A virtual trading web app that lets users trade stocks with real time data. Basic functionality (i.e. registering for an account, getting a stock quote, allowing users to buy/sell stocks, and displaying a user’s stock portfolio) was implemented and submitted to Harvard’s CS50 online course as part...","categories": [],
+        "tags": ["flask","sql","python","api"],
+        "url": "http://localhost:4000/virtual-trading/",
+        "teaser": null
+      },{
+        "title": "Text Prediction Using N-gram Models",
+        "excerpt":"GitHub Repo   Web Application   Exploratory Data Analysis Article Background Around the world, people are spending an increasing amount of time on their mobile devices for a wide range of activities. Microsoft SwiftKey builds smart keyboards that makes it easier for people to generate text on their mobile devices....","categories": [],
+        "tags": ["natural language processing","data science","python"],
+        "url": "http://localhost:4000/text-prediction-ngrams/",
+        "teaser": null
+      }]
